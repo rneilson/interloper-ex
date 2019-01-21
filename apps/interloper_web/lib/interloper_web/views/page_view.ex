af8c2e723fb85797079779ddb6bff79c87657e00
Raymond Neilson <rneilson@interloper.ca>
2019-01-20 23:25:40 -0500

    Moar tweaks

--- apps/interloper_web/lib/interloper_web/views/page_view.ex
+++ apps/interloper_web/lib/interloper_web/views/page_view.ex
@@ -2,6 +2,6 @@ defmodule InterloperWeb.PageView do
   use InterloperWeb, :view
 
   def get_version do
-      Application.spec(:interloper_web, :vsn)
+    Application.spec(:interloper_web, :vsn)
   end
 end
