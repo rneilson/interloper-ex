d7e768fbf0da1223e674ea75be176976bd1792db
Raymond Neilson <rneilson@interloper.ca>
2019-01-26 16:36:40 -0500

    Flesh out some of the required functions, not the GenServer callbacks yet though

--- apps/interloper_web/lib/interloper_web/clients/github.ex
+++ apps/interloper_web/lib/interloper_web/clients/github.ex
@@ -11,6 +11,8 @@ defmodule InterloperWeb.GithubClient do
 
   use GenServer
 
+  @base_url "https://api.github.com"
+
   ## Client
 
   # TODO: guard for path?
@@ -18,26 +20,90 @@ defmodule InterloperWeb.GithubClient do
     GenServer.start_link(__MODULE__, path, name: {:via, InterloperWeb.Registry, get_name(path)})
   end
 
-  # TODO: fetch/1
+  @doc """
+  Retrives (possibly-cached) response from Github API
+  at given `url`, creating new process and updating
+  cached response as required.
+
+  Returns {:ok, response} or {:error, reason}.
+  """
+  @spec fetch(url :: binary) :: {:ok, term} | {:error, term}
+  def fetch(url)
+
+  def fetch(path) when binary_part(path, 0, 1) == "/" do
+    # Ensure server started or get existing
+    case get_or_create_server(path) do
+      {:ok, pid} -> GenServer.call(pid, {:fetch, path})
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def fetch(url) do
+    case get_path(url) do
+      {:ok, path} -> fetch(path)
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
 
   ## Server (callbacks)
 
   def init(path) do
     # Initial state
     # TODO: make this into a struct
-    state = %{ path: path, body: nil, callers: [], cache_tag: nil, cache_valid: false }
+    state = %{ path: path, body: nil, ref: nil, callers: [], cache_tag: nil, cache_valid: false }
+    # TODO: continue?
     {:ok, state}
   end
 
-  ## Internal (utils)
+  # TODO: handle_continue to send first timeout message?
 
-  @doc """
-  Get name tuple for use with registry.
-  """
-  def get_name(path) do
+  # TODO: handle_call/3
+
+  # TODO: handle_info/2
+
+
+  ## Internal (utilities)
+
+  # Get name tuple for use with registries.
+  @spec get_name(binary) :: {atom, binary}
+  defp get_name(path) when is_binary(path) do
     {:github, path}
   end
 
-  # TODO: get_or_create_server/1
+  # Get the path portion of a given Github API URL.
+  # Returns {:ok, path} or {:error, reason}.
+  @spec get_path(binary) :: {:ok, binary} | {:error, term}
+  defp get_path(url)
+
+  # TODO: less-simplistic check?
+  defp get_path(path) when binary_part(path, 0, 1) == "/" do
+    {:ok, path}
+  end
+
+  # TODO: less-simplistic extraction?
+  defp get_path(@base_url <> path) when byte_size(path) > 0 do
+    {:ok, path}
+  end
+
+  defp get_path(url) do
+    {:error, "Invalid Github API URL: #{url}"}
+  end
+
+  # Finds the registered server for `path`, if
+  # it exists, or creates one if not.
+  # Returns {:ok, pid} or {:error, reason}.
+  @spec get_or_create_server(path :: binary) :: {:ok, pid} | {:error, any}
+  defp get_or_create_server(path) do
+    # Try looking up existing process for this path
+    case Registry.lookup(InterloperWeb.Registry, get_name(path)) do
+      [] ->
+        # Spawn a new process for this path
+        DynamicSupervisor.start_child(InterloperWeb.DynamicSupervisor, {__MODULE__, path})
+      [{pid, _} | _] ->
+        # Return first found -- shouldn't be an issue with unique keys
+        {:ok, pid}
+    end
+  end
 
 end
