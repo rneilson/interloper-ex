812f9931eedf6b5b2cc0adf033f8cab58eb468b1
Raymond Neilson <rneilson@interloper.ca>
2019-02-17 12:41:53 -0500

    Scope API helpers

--- apps/interloper_web/lib/interloper_web/router.ex
+++ apps/interloper_web/lib/interloper_web/router.ex
@@ -27,7 +27,7 @@ defmodule InterloperWeb.Router do
     get "/twitter", TwitterController, :recent
   end
 
-  scope "/api", InterloperWeb do
+  scope "/api", InterloperWeb, as: :api do
     pipe_through :api
 
     get "/twitter/recent", TwitterController, :recent
