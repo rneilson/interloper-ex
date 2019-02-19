a42995172ce13d38cbec6e6bdb3830d2bc89e5a0
Raymond Neilson <rneilson@interloper.ca>
2019-02-19 00:19:37 -0500

    Move a whole bunch o' config inside InterloperWeb.Endpoint.init/2

--- apps/interloper_web/lib/interloper_web/endpoint.ex
+++ apps/interloper_web/lib/interloper_web/endpoint.ex
@@ -46,4 +46,90 @@ defmodule InterloperWeb.Endpoint do
   #   signing_salt: "VpTe81l8"
 
   plug InterloperWeb.Router
+
+  # Conditional configuration via OS env vars
+  def init(_key, config) do
+    # URL and port config -- note, assumes base config already in env
+    host =
+      case System.get_env("SITE_NAME") do
+        nil -> Keyword.get(config, :url, []) |> Keyword.get(:host, "localhost")
+        site_name -> site_name
+      end
+    port =
+      case System.get_env("PORT") do
+        nil -> (Keyword.get(config, :http) || []) |> Keyword.get(:port, 4000)
+        port_str -> String.to_integer(port_str)
+      end
+    port_https =
+      case System.get_env("PORT_HTTPS") do
+        nil -> (Keyword.get(config, :https) || []) |> Keyword.get(:port, 4040)
+        port_str -> String.to_integer(port_str)
+      end
+    site_port = System.get_env("SITE_PORT")
+    site_scheme = System.get_env("SITE_SCHEME")
+    # HTTP/S config
+    tls_crt = System.get_env("SITE_TLS_CRT")
+    tls_key = System.get_env("SITE_TLS_KEY") || tls_crt
+    # Return either TLS-enabled or HTTP-only
+    overrides =
+      cond do
+        tls_crt ->
+          # HTTPS, assume redirect
+          url_port =
+            cond do
+              site_port -> String.to_integer(site_port)
+              true -> port_https
+            end
+          # Force port number if nonstandard
+          redir_host =
+            cond do
+              url_port == 443 -> host
+              true -> "#{host}:#{url_port}"
+            end
+          # Set HTTP and HTTPS listeners, force redirect
+          [
+            url: [host: host, port: url_port, scheme: "https"],
+            http: [:inet6, port: port],
+            https: [
+              :inet6,
+              port: port_https,
+              cipher_suite: :strong,
+              certfile: tls_crt,
+              keyfile: tls_key,
+            ],
+            # No HSTS for now until we're stable
+            force_ssl: [hsts: false, host: redir_host]
+          ]
+        site_scheme == "https" ->
+          # Behind TLS-terminating proxy
+          url_port =
+            cond do
+              site_port -> String.to_integer(site_port)
+              true -> port
+            end
+          # Set HTTP, force scheme
+          [
+            url: [host: host, port: url_port],
+            http: [:inet6, port: port],
+            https: false,
+          ]
+        true ->
+          # HTTP-only, direct (probably local)
+          [
+            url: [host: host, port: port],
+            http: [:inet6, port: port],
+            https: false,
+          ]
+      end
+    # Possibly override secret key
+    new_config = 
+      case System.get_env("SECRET_KEY_BASE") do
+        nil ->
+          overrides
+        secret_key_base ->
+          Keyword.put(overrides, :secret_key_base, secret_key_base)
+      end
+    # Merge with provided config
+    {:ok, Keyword.merge(config, new_config)}
+  end
 end
--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,39 +1,13 @@
 ## Runtime configuration
 use Mix.Config
 
-# HTTP/S config
-host = System.get_env("SITE_NAME") || "www.interloper.ca"
-http_port = String.to_integer(System.get_env("SITE_PORT") || "80")
-https_port = String.to_integer(System.get_env("SITE_PORT_HTTPS") || "443")
-tls_crt = System.get_env("SITE_TLS_CRT")
-tls_key = System.get_env("SITE_TLS_KEY") || tls_crt
-# Creds config
+# Internal config
+secret_key_base = System.get_env("SECRET_KEY_BASE")
+# External config
 github_user = System.get_env("GITHUB_USER")
 github_pass = System.get_env("GITHUB_PASS")
-# Other config
-secret_key_base = System.get_env("SECRET_KEY_BASE")
 twitter_base_url = System.get_env("TWITTER_BASE_URL")
 twitter_recent_path = System.get_env("TWITTER_RECENT_PATH")
-
-if tls_crt do
-  # TLS-based setup
-  config :interloper_web, InterloperWeb.Endpoint,
-    url: [host: host, port: https_port],
-    http: [:inet6, port: http_port],
-    https: [
-      :inet6,
-      port: https_port,
-      cipher_suite: :strong,
-      certfile: tls_crt,
-      keyfile: tls_key,
-    ],
-    force_ssl: [hsts: false]  # No HSTS for now until we're stable
-else
-  # HTTP-only setup
-  config :interloper_web, InterloperWeb.Endpoint,
-    http: [:inet6, port: http_port],
-    url: [host: host, port: http_port]
-end
 
 if secret_key_base do
   config :interloper_web, InterloperWeb.Endpoint,
