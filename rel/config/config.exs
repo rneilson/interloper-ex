a57b6b33b4f340160d57ef2d81f50a1c76a35f69
Raymond Neilson <rneilson@interloper.ca>
2019-02-19 23:16:44 -0500

    Use a nice little compaction in the release config

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,35 +1,22 @@
 ## Runtime configuration
 use Mix.Config
 
-# Internal config
-secret_key_base = System.get_env("SECRET_KEY_BASE")
-# External config
-github_user = System.get_env("GITHUB_USER")
-github_pass = System.get_env("GITHUB_PASS")
-twitter_base_url = System.get_env("TWITTER_BASE_URL")
-twitter_recent_path = System.get_env("TWITTER_RECENT_PATH")
-
-if secret_key_base do
-  config :interloper_web, InterloperWeb.Endpoint,
-    secret_key_base: secret_key_base
-end
-
-if github_user do
+# Github config
+if github_user = System.get_env("GITHUB_USER") do
   config :interloper_web, InterloperWeb.GithubClient,
     username: github_user
 end
-
-if github_pass do
+if github_pass = System.get_env("GITHUB_PASS") do
   config :interloper_web, InterloperWeb.GithubClient,
     password: github_pass
 end
 
-if twitter_base_url do
+# Twitter (proxy) config
+if twitter_base_url = System.get_env("TWITTER_BASE_URL") do
   config :interloper_web, InterloperWeb.TwitterController,
     base_url: String.split(twitter_base_url)
 end
-
-if twitter_recent_path do
+if twitter_recent_path = System.get_env("TWITTER_RECENT_PATH") do
   config :interloper_web, InterloperWeb.TwitterController,
     recent_path: twitter_recent_path
 end
