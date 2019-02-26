53a81cdc032dbf04c0a3c0083538101a4b9a4db4
Raymond Neilson <rneilson@interloper.ca>
2019-02-25 19:49:13 -0500

    Update TwitterController config; allow more sensible path setting (ie leading slash)

--- apps/interloper_web/config/config.exs
+++ apps/interloper_web/config/config.exs
@@ -25,8 +25,8 @@ config :interloper_web, InterloperWeb.GithubClient,
 
 # Twitter defaults
 config :interloper_web, InterloperWeb.TwitterController,
-  base_url: ["https://www.interloper.ca/twitter"],
-  recent_path: "recent.json"
+  base_url: ["https://www.interloper.ca"],
+  recent_path: "/api/twitter/recent"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- apps/interloper_web/lib/interloper_web/controllers/twitter_controller.ex
+++ apps/interloper_web/lib/interloper_web/controllers/twitter_controller.ex
@@ -8,7 +8,13 @@ defmodule InterloperWeb.TwitterController do
     # Get URL from env
     env = Application.fetch_env!(:interloper_web, __MODULE__)
     base_url = Keyword.fetch!(env, :base_url) |> Enum.random()
-    url = base_url <> "/" <> Keyword.fetch!(env, :recent_path)
+    path = Keyword.fetch!(env, :recent_path)
+    url =
+      cond do
+        binary_part(path, 0, 1) == "/" -> base_url <> path
+        path == "" -> base_url <> "/"
+        true -> base_url <> "/" <> path
+      end
     case CachingClient.fetch(url) do
       {:ok, recent} ->
         render(conn, :recent, recent: recent)
