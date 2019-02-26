ab3d92763871de8b58e1b33d1320f9a13111fe19
Raymond Neilson <rneilson@interloper.ca>
2019-02-26 00:24:16 -0500

    Add 404, 500 error pages matching site
    Well, *actually*, make a more generic view for future use...

--- apps/interloper_web/lib/interloper_web/views/error_view.ex
+++ apps/interloper_web/lib/interloper_web/views/error_view.ex
@@ -1,11 +1,15 @@
 defmodule InterloperWeb.ErrorView do
   use InterloperWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  alias InterloperWeb.SharedView
+
+  def render("404.html", assigns) do
+    render("err_page.html", Map.put(assigns, :status_code, 404))
+  end
+
+  def render("500.html", assigns) do
+    render("err_page.html", Map.put(assigns, :status_code, 500))
+  end
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
