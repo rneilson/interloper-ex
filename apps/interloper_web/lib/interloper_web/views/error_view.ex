e7253c0e55d5b803a62350017d54a27b5c0e91d3
Raymond Neilson <rneilson@interloper.ca>
2019-02-26 00:35:13 -0500

    Slightly *more* generic...

--- apps/interloper_web/lib/interloper_web/views/error_view.ex
+++ apps/interloper_web/lib/interloper_web/views/error_view.ex
@@ -4,11 +4,11 @@ defmodule InterloperWeb.ErrorView do
   alias InterloperWeb.SharedView
 
   def render("404.html", assigns) do
-    render("err_page.html", Map.put(assigns, :status_code, 404))
+    render("err_page.html", put_error_status(assigns, 404))
   end
 
   def render("500.html", assigns) do
-    render("err_page.html", Map.put(assigns, :status_code, 500))
+    render("err_page.html", put_error_status(assigns, 500))
   end
 
   # By default, Phoenix returns the status message from
@@ -16,5 +16,14 @@ defmodule InterloperWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
+  end
+
+  ## Internal
+
+  # Helper to add status code/text to assigns
+  defp put_error_status(assigns, status_code) do
+    assigns
+    |> Map.put_new(:status_code, status_code)
+    |> Map.put_new(:status_text, Plug.Conn.Status.reason_phrase(status_code))
   end
 end
