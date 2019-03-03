8969dc1b8e5c02bf64919eb87171884adda351f9
Raymond Neilson <rneilson@interloper.ca>
2019-03-02 20:02:59 -0500

    So: we're gonna start with the Interloper application doing something
    Namely, getting a list of connected nodes from the environment, and attempting
    to connect to each on startup

--- apps/interloper/lib/interloper/application.ex
+++ apps/interloper/lib/interloper/application.ex
@@ -6,10 +6,33 @@ defmodule Interloper.Application do
   use Application
 
   def start(_type, _args) do
+    # Connect to any configured peer nodes
+    connect_to_cluster()
+
+    # List all child processes to be supervised
     children = [
       # Interloper.Worker
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Interloper.Supervisor)
+  end
+
+  def get_configured_nodes() do
+    case System.get_env("CONNECT_NODES") do
+      nodes when is_binary(nodes) ->
+        nodes
+        |> String.split()
+        |> Enum.map(fn n -> String.to_atom(n) end)
+      _ ->
+        []
+    end
+  end
+
+  def connect_to_cluster() do
+    this_node = Node.self()
+
+    get_configured_nodes()
+    |> Enum.reject(fn n -> n == this_node end)
+    |> Enum.map(fn n -> {n, Node.connect(n)} end)
   end
 end
